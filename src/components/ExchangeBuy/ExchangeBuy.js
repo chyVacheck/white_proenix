9e1821c3b9cf120d0a0d561fcd328bc027f04e48
Dmytro Shakh <work.dmytro02@gmail.com>
2022-11-28 22:57:45 +0100

    initial cpmponent ExchangeBuy

---
+++ src/components/ExchangeBuy/ExchangeBuy.js
@@ -0,0 +1,13 @@
+
+import './ExchangeBuy.css';
+
+function ExchangeBuy() {
+
+  return (
+    <article className='exchange-buy'>
+      Exchange and Buy
+    </article>
+  )
+}
+
+export default ExchangeBuy;
