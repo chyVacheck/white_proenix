f7ee00d1b2d209893ecad84b15e34b49b83dbd3e
Dmytro Shakh <work.dmytro02@gmail.com>
2022-11-28 16:17:52 +0100

    create component Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,49 @@
+
+import './Footer.css';
+
+import FooterInfoColumn from './__info-column/Footer__info-column';
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer className="footer">
+      <article className='footer__container'>
+        {/* //? колонки с информацией */}
+        {/* //todo вынести в отдельный компонент*/}
+        <div className='footer__info-container'>
+          <FooterInfoColumn
+            title='About us'
+            links={['How it works', 'Fees']}
+          />
+
+          <FooterInfoColumn
+            title='Features'
+            links={['Exchange', 'VPM', 'Track Order', 'Live Prices', 'Coin Specs']}
+          />
+
+          <FooterInfoColumn
+            title='Patrtner with us'
+            links={['Integrate our API', 'Become an Affiliate', 'Share your proposal', 'Start your career']}
+          />
+
+          <FooterInfoColumn
+            title='Policies'
+            links={['Terms of Service', 'Privacy Policy', 'Coockie Policy', 'AML Policy']}
+          />
+
+          <FooterInfoColumn
+            title='Support Center'
+            links={['Explore FAQ', 'Contact Us', 'Sent Feedback']}
+          />
+
+        </div>
+        {/* //? копирайт */}
+        <div className='footer__copyright-container'>
+          <p className='footer__copyright-title'>© {year} Wphoenix.com. All rights reserved</p>
+        </div>
+      </article>
+    </footer>
+  )
+}
+
+export default Footer;
