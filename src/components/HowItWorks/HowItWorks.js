e9e41208d7481e9ef976037cfd298fe4a7927bb4
Dmytro Shakh <work.dmytro02@gmail.com>
2022-11-28 22:57:22 +0100

    initial cpmponent HowItWorks

---
+++ src/components/HowItWorks/HowItWorks.js
@@ -0,0 +1,13 @@
+
+import './HowItWorks.css';
+
+function HowItWorks() {
+
+  return (
+    <article className='how-it-works'>
+      HowItWorks
+    </article>
+  )
+}
+
+export default HowItWorks;
