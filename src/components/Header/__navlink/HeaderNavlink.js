578ed73f0a658c67dc9ddc95578b70efb5ecce6c
Dmytro Shakh <work.dmytro02@gmail.com>
2022-11-29 22:57:39 +0100

    fix: исправить проблему с выделением меню при другом пути
    Раньше при пути 'home' в меню 'Home' не выделялось, сейчас выделяется

--- src/components/Header/__navlink/HeaderNavlink.js
+++ src/components/Header/__navlink/HeaderNavlink.js
@@ -4,7 +4,7 @@ import './HeaderNavlink.css';
 
 function HeaderNavlink({ page, text }) {
   const link = text.split(" ").join("_")
-  const className = ('/' + link) === page ? 'header__navigation-link link header__navigation-link_active' : 'header__navigation-link link';
+  const className = ('/' + link.toLocaleLowerCase()) === page.toLocaleLowerCase() ? 'header__navigation-link link header__navigation-link_active' : 'header__navigation-link link';
 
   return (
     <NavLink className={className} to={`/${link}`}>{text}</NavLink>
