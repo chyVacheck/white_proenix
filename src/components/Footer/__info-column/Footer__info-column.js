1c59831295307e8e40f9facf7d6bf7211ba570d2
Dmytro Shakh <work.dmytro02@gmail.com>
2022-12-01 00:41:06 +0100

    Update Footer__info-column.js

--- src/components/Footer/__info-column/Footer__info-column.js
+++ src/components/Footer/__info-column/Footer__info-column.js
@@ -2,19 +2,17 @@
 import { NavLink } from 'react-router-dom';
 import './footer__info-column.css';
 
-function FooterInfoColumn(props) {
-
-  const links = props.links;
+function FooterInfoColumn({ title, links }) {
 
   return (
     <article className='footer__info-column'>
       <div className='footer__info-title'>
-        {props.title}
+        {title}
       </div>
 
       {links.map((item, index) => {
         return (
-          <NavLink key={index} className='footer__info-link link' to={`/${item.split(" ").join("_")}`}>
+          <NavLink key={index} className='footer__info-link link' to={`${title.split(" ").join("_")}/${item.split(" ").join("_")}`}>
             {item}
           </NavLink>
         )
