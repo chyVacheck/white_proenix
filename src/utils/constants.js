0fb9c0405fd310260182e239355d812c741b69eb
Dmytro Shakh <work.dmytro02@gmail.com>
2022-11-30 15:51:03 +0100

    Create utils Constants and enable icons

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+
+import imageComplete from './../images/icons/complete.svg';
+import imageWaiting from './../images/icons/waiting.svg';
+import imageError from './../images/icons/error.svg';
+
+
+const icons = {
+  complete: imageComplete,
+  waiting: imageWaiting,
+  error: imageError,
+}
+
+
+
+export default icons;
