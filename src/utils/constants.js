b6f9636610d83db753ca224c120c8ad733fb9218
Dmytro Shakh <work.dmytro02@gmail.com>
2022-12-01 00:40:47 +0100

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,11 @@ import imageComplete from './../images/icons/complete.svg';
 import imageWaiting from './../images/icons/waiting.svg';
 import imageError from './../images/icons/error.svg';
 
+//? Pages
+import TermsOfService from './../Pages/TermsOfService.js';
+import PrivacyPolicy from './../Pages/PrivacyPolicy.js';
+import CoockiePolicy from '../Pages/CoockiePolicy';
+import AMLPolicy from '../Pages/AMLPolicy.js';
 
 const images = {
   icon: {
@@ -25,3 +30,20 @@ export const popupInfoContent = {
     error: 'Error',
   }
 }
+
+const Pages = {
+  '/About_us/How_it_works': '',
+  '/About_us/Fees': '',
+
+
+
+  '/Policies/Terms_of_Service': <TermsOfService />,
+  '/Policies/Privacy_Policy': <PrivacyPolicy />,
+  '/Policies/Coockie_Policy': <CoockiePolicy />,
+  '/Policies/AML_Policy': <AMLPolicy />,
+
+
+
+}
+
+export default Pages;
