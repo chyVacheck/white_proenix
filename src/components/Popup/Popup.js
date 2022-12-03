2600ace13128cc450c1c494be6dbcd3eee4844ed
Dmytro Shakh <work.dmytro02@gmail.com>
2022-12-03 14:45:27 +0100

    add component Popup

---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,27 @@
+import './Popup.css';
+
+function Popup({ isOpen, setIsOpen, children, ...props }) {
+
+  function onClose() {
+    setIsOpen(false);
+  }
+
+  const className = props.className + " popup__container"
+
+  return (
+    <section id={`popup`} className={isOpen ? 'popup popup_opened' : 'popup'}>
+      <article id={`popup-container`} className={className}>
+        {children}
+        <button
+          aria-label="закрыть окно pop-up"
+          type="button"
+          id={`popup-button-close`}
+          className="popup__close-button button"
+          onClick={onClose}
+        ></button>
+      </article>
+    </section>
+  )
+}
+
+export default Popup;
