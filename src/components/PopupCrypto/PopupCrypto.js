509af813a682d21a113c8edb897669b8d55024c7
Dmytro Shakh <work.dmytro02@gmail.com>
2022-12-03 14:47:38 +0100

    fear: сделать pop-up crypto

---
+++ src/components/PopupCrypto/PopupCrypto.js
@@ -0,0 +1,69 @@
+
+import './PopupCrypto.css';
+import Popup from './../Popup/Popup.js';
+import CryptoChangeButton from './../CryptoChangeButton/CryptoChangeButton.js';
+import { CryptoContent as images } from '../../utils/constants';
+
+function PopupCrypto({ isOpen, setIsOpen, setCurrentCrypto }) {
+
+  window.PopupCrypto = setIsOpen;
+
+  function closePopup() {
+    setIsOpen(false);
+  }
+
+  return (
+    <Popup
+      isOpen={isOpen}
+      setIsOpen={setIsOpen}
+    >
+      <article className='popupCrypto'>
+
+        <div className='popupCrypto__choose'>
+          <ul className='popupCrypto__list'>
+
+            <CryptoChangeButton
+              crypto={images.BTC}
+              setCurrentCrypto={setCurrentCrypto}
+              closePopup={closePopup}
+            />
+
+            <CryptoChangeButton
+              crypto={images.ETH}
+              setCurrentCrypto={setCurrentCrypto}
+              closePopup={closePopup}
+            />
+            <CryptoChangeButton
+              crypto={images.USDT}
+              setCurrentCrypto={setCurrentCrypto}
+              closePopup={closePopup}
+            />
+            <CryptoChangeButton
+              crypto={images.MATIC}
+              setCurrentCrypto={setCurrentCrypto}
+              closePopup={closePopup}
+            />
+            <CryptoChangeButton
+              crypto={images.NEXO}
+              setCurrentCrypto={setCurrentCrypto}
+              closePopup={closePopup}
+            />
+            <CryptoChangeButton
+              crypto={images.BNB}
+              setCurrentCrypto={setCurrentCrypto}
+              closePopup={closePopup}
+            />
+            <CryptoChangeButton
+              crypto={images.TRX}
+              setCurrentCrypto={setCurrentCrypto}
+              closePopup={closePopup}
+            />
+          </ul>
+        </div>
+
+      </article>
+    </Popup>
+  )
+}
+
+export default PopupCrypto;
