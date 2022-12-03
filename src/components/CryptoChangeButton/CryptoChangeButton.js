18b0be85291732e7420d35f2a02d000f25396038
Dmytro Shakh <work.dmytro02@gmail.com>
2022-12-03 14:50:57 +0100

    add: сделать компонент для pop-up Crypto

---
+++ src/components/CryptoChangeButton/CryptoChangeButton.js
@@ -0,0 +1,31 @@
+
+import './CryptoChangeButton.css';
+
+
+function CryptoChangeButton({ crypto, setCurrentCrypto, closePopup }) {
+
+  function onClick() {
+    setCurrentCrypto(crypto);
+    closePopup();
+  }
+
+  return (
+    <li className='cryptoChangeButton'>
+      <button
+        onClick={onClick}
+        type='button'
+        className='cryptoChangeButton__button'>
+        <img
+          className='cryptoChangeButton__image'
+          alt={crypto.alt}
+          src={crypto.img}
+        />
+        <span className='cryptoChangeButton__name'>{crypto.alt}</span>
+
+      </button>
+      <span className='cryptoChangeButton__fullname'>{crypto.name}</span>
+    </li>
+  )
+}
+
+export default CryptoChangeButton;
