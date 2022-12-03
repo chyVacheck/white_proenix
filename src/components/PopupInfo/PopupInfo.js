fff723827c399c33d5da9ffc3a4cf1d2db1e286d
Dmytro Shakh <work.dmytro02@gmail.com>
2022-12-03 14:47:03 +0100

    refactor: переделать pop-up info под новый компоненты

--- src/components/PopupInfo/PopupInfo.js
+++ src/components/PopupInfo/PopupInfo.js
@@ -1,32 +1,26 @@
 
 import './PopupInfo.css';
+import Popup from './../Popup/Popup.js';
 
-function PopupInfo({ isOpen, setIsOpen, img, alt, message, onClose }) {
+function PopupInfo({ isOpen, setIsOpen, img, alt, message }) {
 
-  function onClick() {
-    onClose(isOpen, setIsOpen);
-  }
+  window.PopupInfo = setIsOpen;
 
   return (
-    <section id={`popupInfo`} className={isOpen ? 'popupInfo popupInfo_opened' : 'popupInfo'}>
-      <article id={`popupInfo-container`} className="popupInfo__container popupInfo__container_biger">
-        <img
-          src={img}
-          className="popupInfo__img"
-          alt={alt}
-        />
-        <p className="popupInfo__message">
-          {message}
-        </p>
-        <button
-          aria-label="закрыть окно pop-up"
-          type="button"
-          id={`popupInfo-button-close`}
-          className="popupInfo__close-button button"
-          onClick={onClick}
-        ></button>
-      </article>
-    </section>
+    <Popup
+      isOpen={isOpen}
+      setIsOpen={setIsOpen}
+      className={'popupInfo'}
+    >
+      <img
+        className="popupInfo__img"
+        src={img}
+        alt={alt}
+      />
+      <p className="popupInfo__message">
+        {message}
+      </p>
+    </Popup>
   )
 }
 
