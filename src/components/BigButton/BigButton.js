6a0b80b004555e112de79ce0ec24312589daeb0a
Dmytro Shakh <work.dmytro02@gmail.com>
2022-12-03 15:02:02 +0100

    Update BigButton.js

--- src/components/BigButton/BigButton.js
+++ src/components/BigButton/BigButton.js
@@ -1,10 +1,15 @@
 
 import './BigButton.css';
 
-function BigButton({ type, sizeBig, children }) {
+function BigButton({ func = ((str = '') => { console.log(str) }), type, sizeBig, children }) {
+
+  function onClick() {
+    func(children);
+  }
 
   return (
     <button
+      onClick={onClick}
       className={`bigButton button ${sizeBig ? '' : 'bigButton_medium'}`}
       type={type}
     >
