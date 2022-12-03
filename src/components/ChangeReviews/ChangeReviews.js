693015890d253f9f1d7aa986417b8a3a1aca6c2f
Dmytro Shakh <work.dmytro02@gmail.com>
2022-12-03 14:53:04 +0100

    add: добавть компонент для компонента Reviews

---
+++ src/components/ChangeReviews/ChangeReviews.js
@@ -0,0 +1,35 @@
+
+import './ChangeReviews.css';
+
+function ChangeReviews({ reviews = [], setLeftReview, leftReview, setRightReview, rightReview, isOpening, setIsOpening }) {
+
+
+  return (
+    <article className='changeReviews'>
+      {reviews.map((item, index) => {
+
+        function onClick() {
+          setLeftReview(item);
+          if (index === reviews.length - 1) {
+            setRightReview(reviews[0]);
+          } else {
+            setRightReview(reviews[index + 1]);
+          }
+          setIsOpening(!isOpening);
+        }
+
+        return (
+          <button
+            onClick={onClick}
+            type='button'
+            key={index}
+            className={(item === leftReview) || (item === rightReview) ? 'changeReviews__button_active changeReviews__button button' : 'changeReviews__button button'}
+          />
+        )
+      })
+      }
+    </article >
+  )
+}
+
+export default ChangeReviews;
