6b97b35c90da7cc629d33d7ceeb2c6f05b8bce6a
Dmytro Shakh <work.dmytro02@gmail.com>
2022-12-08 14:59:57 +0100

    feat: сделать страницу поддержки
    Пока что тут будут данные о том куда писать в случае проблем, в будущем здесь будет диалоговое окно

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -1,11 +1,34 @@
 
+import { NavLink } from 'react-router-dom';
 import './Support.css';
 
 function Support() {
 
+  const email = 'whiteproenix@gmail.com';
+
+  function onClick() {
+    navigator.clipboard.writeText(email);
+    console.log('copied');
+  }
+
   return (
     <article className='support'>
-      Support
+      <div className='support__container'>
+        <p className='support__messanger'>
+          Please write <span
+            onClick={onClick}
+            className='support__messanger support__messanger_link link'>
+            {email}
+          </span>
+        </p>
+        <p className='support__messanger'>
+          Or write to us in a <NavLink
+            className='support__messanger support__messanger_link link'
+            to={'https://'}>
+            telegram
+          </NavLink>
+        </p>
+      </div>
     </article>
   )
 }
