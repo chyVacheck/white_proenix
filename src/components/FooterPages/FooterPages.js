d447b47507c86547a1194a1e5a31d1b43326edf3
Dmytro Shakh <work.dmytro02@gmail.com>
2022-12-08 22:35:55 +0100

    fix: исправить "пустые страницы"

--- src/components/FooterPages/FooterPages.js
+++ src/components/FooterPages/FooterPages.js
@@ -5,13 +5,15 @@ import './FooterPages.css';
 import { useLocation } from 'react-router-dom';
 import Pages from '../../utils/constants';
 
+import PageNotFound from './../../Pages/Info/PageNotFound.js';
+
 function FooterPages() {
 
   let location = useLocation();
   const page = location.pathname;
 
   return (
-    Pages[page]
+    Pages[page] ? Pages[page] : <PageNotFound />
   )
 
 }
