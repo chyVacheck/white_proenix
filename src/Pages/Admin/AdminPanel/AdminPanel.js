48fee74602c35ab097986e3cfca7233e6331b92a
Dmytro Shakh <work.dmytro02@gmail.com>
2022-12-11 00:11:54 +0100

    Update AdminPanel

--- src/Pages/Admin/AdminPanel/AdminPanel.js
+++ src/Pages/Admin/AdminPanel/AdminPanel.js
@@ -5,17 +5,23 @@ import './AdminPanel.css';
 // личные
 import AllUsers from './AllUsers/AllUsers.jsx';
 import AllTransactionHistory from './AllTransactionHistory/AllTransactionHistory.jsx';
+import CurrentCryptocurrencyRate from './CurrentCryptocurrencyRate/CurrentCryptocurrencyRate';
 import CurrentTransaction from './CurrentTransaction/CurrentTransaction.jsx';
 
 function AdminPanel({ popupValid }) {
 
   return (
     <section className='adminPanel'>
-      {/*//? история транзакций */}
-      <AllTransactionHistory class='adminPanel__container' />
+      <div className='adminPanel__column'>
+        {/*//? текущий курс на бинансе и на сайте */}
+        <CurrentCryptocurrencyRate class='adminPanel__container' />
+
+        {/*//? история транзакций */}
+        <AllTransactionHistory class='adminPanel__container' />
+      </div>
 
       {/*//? активные транзакции */}
-      <CurrentTransaction class='adminPanel__container' />
+      <CurrentTransaction popupValid={popupValid} class='adminPanel__container' />
 
       {/*//? все пользователи */}
       <AllUsers popupValid={popupValid} class='adminPanel__container' />
